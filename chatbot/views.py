194d2145ef9f837e68bae026b4b460d065896ad3
sphinx-austin <austinjuniour4@gmail.com>
2023-09-05 13:09:34 +0300

    login auth

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+# third party imports
+from django.http import JsonResponse
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'base.html')
+
+
+# AUTHENTICATION
+# login
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatbot')
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_messahe': error_message})
+    else:
+        return render(request, 'login.html')
