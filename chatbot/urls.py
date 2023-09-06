b1266593d95992f321106cd0605264a93e2857eb
sphinx-austin <austinjuniour4@gmail.com>
2023-09-06 14:00:34 +0300

    update

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name='home')
+    # path("", views.home, name='home'),
+    path("", views.chatbot, name='chatbot'),
+    path("register/", views.register, name='register'),
+    path("login/", views.login, name='login'),
+    path("logout/", views.logout, name='logout'),
 ]
